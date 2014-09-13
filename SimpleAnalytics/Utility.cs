9b046a7fb16d67904c1d562f16289c2e600abba5
Raphael Mun <raphael.mun@outlook.com>
2014-09-13 19:06:35 +0000

    SimpleAnalytics initial tests written

---
+++ SimpleAnalytics/Utility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleAnalytics
+{
+    public static class SystemTime
+    {
+        public static DateTime Now
+        {
+            get { return NowFunc(); }
+        }
+
+        public static DateTime UtcNow
+        {
+            get { return UtcNowFunc(); }
+        }
+
+        public static Func<DateTime> NowFunc = () => DateTime.Now;
+        public static Func<DateTime> UtcNowFunc = () => DateTime.UtcNow;
+
+        public static void ResetToDefault()
+        {
+            Func<DateTime> NowFunc = () => DateTime.Now;
+            Func<DateTime> UtcNowFunc = () => DateTime.UtcNow;
+        }
+    }
+
+    public static class Utility
+    {
+        public static string GenerateUUID()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ SimpleAnalyticsTests/WhenUsingEvents.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleAnalytics;
+
+namespace SimpleAnalyticsTests
+{
+    [TestClass]
+    public class WhenUsingEvents
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            SystemTime.ResetToDefault();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            SystemTime.ResetToDefault();
+        }
+
+        [TestMethod]
+        public void ConstructorSetsDefaultName()
+        {
+            Events testEvent = new Events();
+            Assert.AreEqual( "Default", testEvent.Name );
+        }
+
+        [TestMethod]
+        public void ConstructorSetsDetails()
+        {
+            Events testEvent = new Events( new Dictionary<string, string>() { { "Test", "Test" } } );
+            Assert.AreEqual( "Test", testEvent.Details[ "Test" ] );
+        }
+
+        [TestMethod]
+        public void ConstructorSetsNameFromDetails()
+        {
+            Events testEvent = new Events( new Dictionary<string, string>() { { "Name", "Test" } } );
+            Assert.AreEqual( "Test", testEvent.Name );
+        }
+
+        [TestMethod]
+        public void ConstructorSetsSessionID()
+        {
+            Events testEvent = new Events();
+            Assert.IsFalse( string.IsNullOrEmpty( testEvent.SessionID ) );
+        }
+
+        [TestMethod]
+        public void NameSetsName()
+        {
+            Events testEvent = new Events();
+            testEvent.Name = "Test";
+            Assert.AreEqual( "Test", testEvent.Name );
+        }
+
+        [TestMethod]
+        public void NameInDetailSetsName()
+        {
+            Events testEvent = new Events();
+            testEvent.Detail( new Dictionary<string, string>() { { "Name", "Test" } } );
+            Assert.AreEqual( "Test", testEvent.Name );
+        }
+
+        [TestMethod]
+        public void IncrementCreatesNewEvent()
+        {
+            Events testEvent = new Events();
+            testEvent.Increment( "TestEvent" );
+            Assert.IsNotNull( testEvent[ "TestEvent" ] );
+        }
+
+        [TestMethod]
+        public void IncrementAddsCountToEvent()
+        {
+            Events testEvent = new Events();
+            testEvent.Increment( "TestEvent" );
+            Assert.AreEqual( 1, testEvent[ "TestEvent" ].Count );
+        }
+
+        [TestMethod]
+        public void OpenCreatesNewEvent()
+        {
+            Events testEvent = new Events();
+            testEvent.Open( "TestEvent" );
+            Assert.IsNotNull( testEvent[ "TestEvent" ] );
+        }
+
+        [TestMethod]
+        public void CloseAddsToTheListOfOccurances()
+        {
+            Events testEvent = new Events();
+            string id = testEvent.Open( "TestEvent" );
+            SystemTime.UtcNowFunc = () => DateTime.UtcNow + TimeSpan.FromSeconds( 5 );
+            testEvent.Close( id );
+            Assert.AreEqual( 1, testEvent[ "TestEvent" ].Count );
+        }
+
+        [TestMethod]
+        public void CloseUpdatesEventValue()
+        {
+            Events testEvent = new Events();
+            string id = testEvent.Open( "TestEvent" );
+            SystemTime.UtcNowFunc = () => DateTime.UtcNow + TimeSpan.FromSeconds( 5 );
+            testEvent.Close( id );
+
+            EventOccurance occurance = testEvent[ "TestEvent" ].Occurances.Last();
+            Assert.IsTrue( occurance.FinishedTime > occurance.Time );
+        }
+
+        [TestMethod]
+        [ExpectedException( typeof( ArgumentException ) )]
+        public void CloseFailsForNonexistentEventID()
+        {
+            Events testEvent = new Events();
+            testEvent.Close( Utility.GenerateUUID() );
+        }
+
+        [TestMethod]
+        public void FlushExpiresOldOpenEvents()
+        {
+            Events testEvent = new Events();
+            string id = testEvent.Open( "TestEvent", 5 );
+            SystemTime.UtcNowFunc = () => DateTime.UtcNow + TimeSpan.FromSeconds( 10 );
+            testEvent.Flush();
+
+            Assert.AreEqual( 0, testEvent[ "TestEvent" ].Count );
+            Assert.AreEqual( 0, testEvent[ "TestEvent" ].OpenCount );
+        }
+
+        [TestMethod]
+        public void NonexistentEventNameReturnsNull()
+        {
+            Events testEvent = new Events();
+            Assert.IsNull( testEvent[ "TestEvent" ] );
+        }
+
+        [TestMethod]
+        public void ExpiredEventsAreAddedToExpiredCount()
+        {
+            Events testEvent = new Events();
+            string id = testEvent.Open( "TestEvent", 5 );
+            SystemTime.UtcNowFunc = () => DateTime.UtcNow + TimeSpan.FromSeconds( 10 );
+            testEvent.Flush();
+
+            Assert.AreEqual( 1, testEvent[ "TestEvent" ].ExpiredCount );
+        }
+    }
+}
